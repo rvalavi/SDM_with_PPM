2aba5e28864a80d6743cc67d47cd403046d65038
Roozbeh Valavi <valavi.r@gmail.com>
2022-12-07 20:25:09 +1100

    cleaning repo and data clean function

--- R/helper_functions.R
+++ R/helper_functions.R
@@ -3,3 +3,35 @@ as.im.SpatRaster <- function (from, factor.col.name = NULL){
   im <- maptools::as.im.RasterLayer(raster::raster(from))
   return(im)
 }
+
+points <- cleaned_data
+r <- raster_covars[[1]]
+
+clean_occ <- function(
+    points, # dataframe of coordinates of occurrences (e.g., logitude and latitude)
+    r, # one raster covariate to be used as a mask
+    # jitter = 0,
+    remove_outside = TRUE, # remove points outside of window
+    remove_duplicates = TRUE #remove duplicate points
+){
+  # for considering jitter look at sf::st_jitter()
+  r <- r[[1]]
+  
+  # revise this; as in PPM the cell duplicates doesn't matter
+  if(remove_duplicates){
+    cells <- terra::cellFromXY(r, as.matrix(points))
+    points <- points[!duplicated(cells),]
+  }
+  
+  if(remove_outside){
+    imr <- as.im(r)
+    win <- spatstat.geom::as.owin(imr)
+    is_inside <- inside.owin(x = points[,1], 
+                             y = points[,2],
+                             w = win)
+    points <- points[is_inside, ]
+  }
+  
+  return(points)
+}
+
