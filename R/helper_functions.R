6b7c1bdb99911ca2fa1f59fa70b82003ded9b0fa
Roozbeh Valavi <valavi.r@gmail.com>
2022-12-08 22:16:44 +1100

    add pkgs

--- R/helper_functions.R
+++ R/helper_functions.R
@@ -1,3 +1,8 @@
+library(sf)
+library(spatstat)
+library(terra)
+
+
 # Note: to generalise the maptools::as.im.RasterLayer to terra::SpatRaster
 as.im.SpatRaster <- function (from, factor.col.name = NULL){
   im <- maptools::as.im.RasterLayer(raster::raster(from))
